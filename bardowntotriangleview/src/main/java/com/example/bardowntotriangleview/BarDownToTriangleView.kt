19e4f03abb07890f286b28b470dab8bf51e06b23
Anwesh43 <anweshthecool0@gmail.com>
2021-03-28 03:12:21 +0530

    adding constants and libraries

--- bardowntotriangleview/src/main/java/com/example/bardowntotriangleview/BarDownToTriangleView.kt
+++ bardowntotriangleview/src/main/java/com/example/bardowntotriangleview/BarDownToTriangleView.kt
@@ -1,2 +1,26 @@
 package com.example.bardowntotriangleview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Path
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#3F51B5",
+    "#004D40",
+    "#4A148C",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val sizeFactor : Float = 2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
