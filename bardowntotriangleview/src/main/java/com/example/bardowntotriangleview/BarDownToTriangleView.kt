e2bdb23abfb12bd7120519b7d37073a9873ed62d
Anwesh43 <anweshthecool0@gmail.com>
2021-03-28 03:27:14 +0530

    adding Canvas extension methods

--- bardowntotriangleview/src/main/java/com/example/bardowntotriangleview/BarDownToTriangleView.kt
+++ bardowntotriangleview/src/main/java/com/example/bardowntotriangleview/BarDownToTriangleView.kt
@@ -29,3 +29,48 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBarDownToTriangle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    drawRect(
+        RectF(
+            -w / 2,
+            -h / 2 + h * sf2,
+            w / 2,
+            -h / 2 + h * (sf1)
+        ),
+        paint
+    )
+    save()
+    translate(-w / 2, h / 2)
+    val path : Path = Path()
+    path.moveTo(0f, 0f)
+    path.lineTo(size / 2, -size / 2)
+    path.lineTo(w, 0f)
+    path.lineTo(0f, 0f)
+    clipPath(path)
+    drawRect(
+        RectF(
+            0f,
+            -size * sf3,
+            w,
+            0f
+        ),
+        paint
+    )
+    restore()
+    restore()
+}
+
+fun Canvas.drawaBDTTNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBarDownToTriangle(scale, w, h, paint)
+}
