8b12fa64cd96a2a42eeab505eaf6b54a11f98dc6
Anwesh43 <anweshthecool0@gmail.com>
2021-03-28 03:48:13 +0530

    adding BarDownToTriangle class

--- bardowntotriangleview/src/main/java/com/example/bardowntotriangleview/BarDownToTriangleView.kt
+++ bardowntotriangleview/src/main/java/com/example/bardowntotriangleview/BarDownToTriangleView.kt
@@ -178,4 +178,27 @@ class BarDownToTriangleView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BarDownToTriangle(var i : Int) {
+
+        private var curr : BDTTNode = BDTTNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
